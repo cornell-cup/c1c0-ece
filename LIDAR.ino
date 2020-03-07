1aa40e03fee8bd4a6979f867e476c3c86db49e6f
spegels62 <47483575+spegels62@users.noreply.github.com>
2020-03-07 15:37:01 -0500

    Added parsing lidar data into a continuously refleshing 360 element list of tuples containing angle and distance.

--- LIDAR.ino
+++ LIDAR.ino
@@ -1,3 +1,4 @@
+
 /*
  * RoboPeak RPLIDAR Arduino Example
  * This example shows the easy and common way to fetch data from an RPLIDAR
@@ -37,10 +38,10 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
- 
+
 // This sketch code is based on the RPLIDAR driver library provided by RoboPeak
-//https://github.com/robopeak/rplidar_arduino Library destination for setup ~IMPORTANT~
 #include <RPLidar.h>
+
 
 // You need to create an driver instance 
 RPLidar lidar;
@@ -52,7 +53,7 @@ void setup() {
   
   // bind the RPLIDAR driver to the arduino hardware serial
  Serial1.begin(115200);
- Serial.begin(9600);
+ Serial.begin(4800);
  Serial.println("Here");
  lidar.begin(Serial1);
  Serial.println("Here");
@@ -60,34 +61,42 @@ void setup() {
   
   // set pin modes
   pinMode(RPLIDAR_MOTOR, OUTPUT);
-}
-
-void loop() {
   
-  if (IS_OK(lidar.waitPoint())) {
-    float distance = lidar.getCurrentPoint().distance; //distance value in mm unit
-    float angle    = lidar.getCurrentPoint().angle; //anglue value in degree
-    bool  startBit = lidar.getCurrentPoint().startBit; //whether this point is belong to a new scan
-    byte  quality  = lidar.getCurrentPoint().quality; //quality of the current measurement
-    
-    //perform data processing here... 
-    Serial.print("distance: ");
-    Serial.println(distance);
-    Serial.println("angle: ");
-    Serial.println(angle);
-    
-  } else {
-    analogWrite(RPLIDAR_MOTOR, 0); //stop the rplidar motor
-    
-    // try to detect RPLIDAR... 
-    rplidar_response_device_info_t info;
-    if (IS_OK(lidar.getDeviceInfo(info, 100))) {
-       // detected...
-       lidar.startScan();
-       // start motor rotating at max allowed speed
-       analogWrite(RPLIDAR_MOTOR, 255);
-       delay(1000);
+  
+}
+int count = 0;
+typedef struct tuple{ float angle; float distance;}tuple;
+int i = 0;
+tuple LidarData[360];//replace with fixed length and clear/run again if needed
+void loop() {
+    i = i%360;
+    if (IS_OK(lidar.waitPoint())) {
+        float distance = lidar.getCurrentPoint().distance; //distance value in mm unit
+        float angle    = lidar.getCurrentPoint().angle; //angle value in degrees
+        LidarData[i] = tuple{angle,distance};
+        
+        Serial.println(LidarData[i].angle);
+        Serial.println(LidarData[i].distance);
+        i++;
+        
+//        bool  startBit = lidar.getCurrentPoint().startBit; //whether this point is belong to a new scan
+//        byte  quality  = lidar.getCurrentPoint().quality; //quality of the current measurement
+
+
+      //perform data processing here... 
+      
+    } else {
+      analogWrite(RPLIDAR_MOTOR, 0); //stop the rplidar motor
+      
+      // try to detect RPLIDAR... 
+      rplidar_response_device_info_t info;
+      if (IS_OK(lidar.getDeviceInfo(info, 100))) {
+         // detected...
+         lidar.startScan();
+         // start motor rotating at max allowed speed
+         analogWrite(RPLIDAR_MOTOR, 255);
+         delay(1000);
+      }
     }
-  }
 
 }
