fcf0680ae8a1227a07f6a72b918e022ebc849948
c1c0-main <jwt95@cornell.edu>
2021-10-02 14:57:29 -0400

    add LIDAR_API.py file

---
+++ LIDAR_API.py
@@ -0,0 +1,47 @@
+import serial
+import time
+import sys
+
+"""
+To be extended into Lidar API, maybe extended for terabees/IMU as well
+
+"""
+sys.path.append('../c1c0-movement/c1c0-movement/Locomotion')
+import R2Protocol2 as r2p
+
+ser = serial.Serial('/dev/ttyTHS1',
+					9600)
+					
+
+print(ser)
+
+def pack(tup):
+	return(tup[0]<<8 | tup[1])
+	
+
+try:
+	while True:
+		ser_msg = ser.read(216)
+			
+		mtype, lidar_data, status = r2p.decode(ser_msg)
+		#print(mtype, msg, status)
+		
+		for i in range(0, len(lidar_data), 4):
+			#print("Here")
+			angle_msbs = lidar_data[i]
+			angle_lsbs = lidar_data[i+1]
+			distance_msbs = lidar_data[i+2]
+			distance_lsbs = lidar_data[i+3]
+			print("Packet: " + str(i))
+			print("Angle MSB: " + str(angle_msbs) + " Angle LSB: " + str(angle_lsbs))
+			print("Distance MSB: " + str(distance_msbs) + " Distance LSB: " + str(distance_lsbs))
+			angle = pack((angle_msbs, angle_lsbs))
+			distance = pack((distance_msbs, angle_msbs))
+			print("Angle: " + str(angle))
+			print("Distance: " + str(distance))
+			print("")
+		
+except KeyboardInterrupt:
+	ser.close()
+
+	
