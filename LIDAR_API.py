2e1f67d8a6003ca4df0768b183f05902161b5fef
c1c0-main <jwt95@cornell.edu>
2021-10-08 18:46:02 -0400

    Lidar API encapsulated

--- LIDAR_API.py
+++ LIDAR_API.py
@@ -3,54 +3,92 @@ import time
 import sys
 
 """
-To be extended into Lidar API, maybe extended for terabees/IMU as well
+LIDAR API for use with path_planning. 
 
 """
-sys.path.append('../c1c0-movement/c1c0-movement/Locomotion')
+sys.path.append('../c1c0-movement/c1c0-movement/Locomotion') #Might need to be resolved
 import R2Protocol2 as r2p
 
-ser = serial.Serial('/dev/ttyTHS1',
-					9600)
-startseq = (16777215).to_bytes(3, 'big')
-endseq = (16777214).to_bytes(3, 'big')
+ser = None
+startseq = None
+endseq = None
+lidar_tuple_array = []
 
-print(ser)
+def init_serial(port, baud):
+	"""
+	Opens serial port for LIDAR communication
+	port should be a string linux port: Ex dev/ttyTHS1
+	Baud is int the data rate, commonly multiples of 9600
+	"""
+	global ser, startseq, endseq
+	
+	ser = serial.Serial('/dev/ttyTHS1',
+						9600)
+	startseq = (16777215).to_bytes(3, 'big')
+	endseq = (16777214).to_bytes(3, 'big')
+
+
 
 def pack(tup):
 	return(tup[0]<<8 | tup[1])
 	
 
-try:
-	while True:
-		ser_msg = ser.read(444)
-			
-		mtype, lidar_data, status = r2p.decode(ser_msg)
-		#print(mtype, msg, status
-		
-		print(lidar_data)
-		
-		start = lidar_data.index(startseq) + 3
-		end = lidar_data.index(endseq, start)
-		
-		lidar_data = lidar_data[start:end]
-		
-		
-		for i in range(0, len(lidar_data), 4):
-			#print("Here")
-			angle_msbs = lidar_data[i]
-			angle_lsbs = lidar_data[i+1]
-			distance_msbs = lidar_data[i+2]
-			distance_lsbs = lidar_data[i+3]
-			print("Packet: " + str(i))
-			print("Angle MSB: " + str(angle_msbs) + " Angle LSB: " + str(angle_lsbs))
-			print("Distance MSB: " + str(distance_msbs) + " Distance LSB: " + str(distance_lsbs))
-			angle = pack((angle_msbs, angle_lsbs))
-			distance = pack((distance_msbs, angle_msbs))
-			print("Angle: " + str(angle))
-			print("Distance: " + str(distance))
-			print("")
-		
-except KeyboardInterrupt:
-	ser.close()
-
+def get_LIDAR_tuples():
+	"""
+	Acquires and returns one array of lidar data:
+	Data is acquired in a tuple array of 50 data points
+	The first entry in the tuple is an angle in [0,359]
+	The second entry in the tuple is the distance, in mm, floored to nearest int
 	
+	"""
+	global ser, startseq, endseq, lidar_tuple_array
+	
+	lidar_tuple_array = []
+	ser_msg = ser.read(444)
+		
+	mtype, lidar_data, status = r2p.decode(ser_msg)
+	#print(mtype, msg, status
+	
+	#print(lidar_data)
+	
+	start = lidar_data.index(startseq) + 3
+	end = lidar_data.index(endseq, start)
+	
+	lidar_data = lidar_data[start:end]
+	print(len(lidar_data))
+	
+	for i in range(0, len(lidar_data), 4):
+		#print("Here")
+		angle_msbs = lidar_data[i]
+		angle_lsbs = lidar_data[i+1]
+		distance_msbs = lidar_data[i+2]
+		distance_lsbs = lidar_data[i+3]
+		#print("Packet: " + str(i))
+		#print("Angle MSB: " + str(angle_msbs) + " Angle LSB: " + str(angle_lsbs))
+		#print("Distance MSB: " + str(distance_msbs) + " Distance LSB: " + str(distance_lsbs))
+		angle = pack((angle_msbs, angle_lsbs))
+		distance = pack((distance_msbs, angle_msbs))
+		#print("Angle: " + str(angle))
+		#print("Distance: " + str(distance))
+		#print("")
+		lidar_tuple_array.append((angle,distance))
+	
+	return lidar_tuple_array
+		
+
+
+if __name__ == '__main__':
+	init_serial('/dev/ttyTHS1', 9600)
+	
+	try:
+		while True:
+			start = time.time()
+			arr = get_LIDAR_tuples()
+			for i in arr:
+				print(i)
+			print("End of seg")
+			end = time.time() - start
+			print(end)
+			
+	except KeyboardInterrupt:
+		ser.close()
