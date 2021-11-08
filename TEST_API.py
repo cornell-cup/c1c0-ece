a9864620071dedf883bbd36e5dfe32668799eb0b
c1c0-main <jwt95@cornell.edu>
2021-11-08 11:15:35 -0500

    TEST_API.py

--- TEST_API.py
+++ TEST_API.py
@@ -15,14 +15,14 @@ terabee_array_1 = []
 terabee_array_2 = []
 terabee_array_3 = []
 ldr_array = []
-imu_gyro_array = []
-imu_accel_array = []
+imu_array = []
 
 def init_serial(port, baud):
 	"""
 	Opens serial port for LIDAR communication
 	port should be a string linux port: Ex dev/ttyTHS1
 	Baud is int the data rate, commonly multiples of 9600
+	For using all all three types of sensors, baud should be 115200
 	"""
 	global ser, startseq, endseq
 	
@@ -31,6 +31,9 @@ def init_serial(port, baud):
 	
 
 def close_serial():
+	"""
+	Closes serial port for sensor communication
+	"""
 	global ser
 	ser.close()
 	
@@ -48,10 +51,8 @@ def get_array(array_type):
 		return terabee_array_3
 	elif array_type == "LDR":
 		return ldr_array
-	elif array_type == "IMUG":
-		return imu_gyro_array
-	elif array_type == "IMUA":
-		return imu_accel_array
+	elif array_type == "IMU":
+		return imu_array
 	else:
 		print("array_type must be one of 'TB1', 'TB2', 'TB3', 'LDR', IMUG', or 'IMUA'" )
 	
@@ -65,20 +66,19 @@ def decode_arrays():
 	
 	"""
 	global ser, terabee_array_1, terabee_array_2, terabee_array_3, ldr_array
-	global imu_gyro_array, imu_accel_array
+	global imu_array
 	
 	good_data = False
 	print("GET ARRAY FUNCTION")
 	while(not good_data):
 		terabee_array_1 = []
 		terabee_array_2 = []
-		#terabee_array_3 = []
+		terabee_array_3 = []
 		ldr_array = []
-		imu_gyro_array = []
-		imu_accel_array = []
+		imu_array = []
 		
 		print("IN LOOOP")
-		ser_msg = ser.read(308)
+		ser_msg = ser.read(334)
 		print("GOT MESSAGE")
 		mtype, data, status = r2p.decode(ser_msg)
 		
@@ -91,6 +91,10 @@ def decode_arrays():
 			
 		elif (mtype == b'IR2\x00'):
 			decode_from_ir2(data)
+			good_data = True
+			
+		elif (mtype == b'IR3\x00'):
+			decode_from_ir3(data)
 			good_data = True
 			
 		elif (mtype == b'LDR\x00'):
@@ -116,31 +120,15 @@ def decode_from_ir(data):
 	
 	terabee1_data = data[0:16]
 	terabee2_data = data[32:32+16]
-	ldr_data = data[64:64+200]
-	imu_data = data[264+16:]
+	terabee3_data = data[64:64+16]
+	ldr_data = data[96:96+200]
+	imu_data = data[296+16:]
 	
 	terabee_array_append(terabee1_data, terabee_array_1)
 	terabee_array_append(terabee2_data, terabee_array_2)
+	terabee_array_append(terabee3_data, terabee_array_3)
 	lidar_tuple_array_append(ldr_data, ldr_array)
-	imu_array_append(imu_data[:6], imu_gyro_array)
-	imu_array_append(imu_data[6:], imu_accel_array)
-	
-	print("TERABEE 1:")
-	for item in terabee_array_1:
-		print(item)
-	
-	print("TERABEE 2:")
-	for item in terabee_array_2:
-		print(item)
-		
-	print("LIDAR:")
-	print(ldr_array)
-	
-	print("IMU GYRO: ")
-	print(imu_gyro_array)
-	
-	print("IMU ACCEL: ")
-	print(imu_accel_array)
+	imu_array_append(imu_data, imu_array)
 		
 def decode_from_ir2(data):
 	"""
@@ -150,32 +138,36 @@ def decode_from_ir2(data):
 	"""
 	
 	terabee2_data = data[0:16]
-	ldr_data = data[32:32+200]
-	imu_data = data[232+16:232+16+12]
-	terabee1_data = data[260+16:]
+	terabee3_data = data[32:32+16]
+	ldr_data = data[64:64+200]
+	imu_data = data[280:280+6]
+	terabee1_data = data[286+16:]
 	
 	terabee_array_append(terabee1_data, terabee_array_1)
 	terabee_array_append(terabee2_data, terabee_array_2)
+	terabee_array_append(terabee3_data, terabee_array_3)
 	lidar_tuple_array_append(ldr_data, ldr_array)
-	imu_array_append(imu_data[:6], imu_gyro_array)
-	imu_array_append(imu_data[6:], imu_accel_array)
+	imu_array_append(imu_data, imu_array)
+
+def decode_from_ir3(data):
+	"""
+	Description: Function that decodes the data if the received mtype
+	is that corresponding to Terabee 3.
+	Returns: Nothing
+	"""
 	
-	print("TERABEE 1:")
-	for item in terabee_array_1:
-		print(item)
+	terabee3_data = data[0:16]
+	ldr_data = data[32:32+200]
+	imu_data = data[248:248+12]
+	terabee1_data = data[270:270+16]
+	terabee2_data = data[286+16:]
 	
-	print("TERABEE 2:")
-	for item in terabee_array_2:
-		print(item)
-		
-	print("LIDAR:")
-	print(ldr_array)
-	
-	print("IMU GYRO: ")
-	print(imu_gyro_array)
-	
-	print("IMU ACCEL: ")
-	print(imu_accel_array)
+	terabee_array_append(terabee1_data, terabee_array_1)
+	terabee_array_append(terabee2_data, terabee_array_2)
+	terabee_array_append(terabee3_data, terabee_array_3)
+	lidar_tuple_array_append(ldr_data, ldr_array)
+	imu_array_append(imu_data, imu_array)
+
 	
 def decode_from_ldr(data):
 	"""
@@ -184,33 +176,18 @@ def decode_from_ldr(data):
 	Returns: Nothing
 	"""
 	
-	ldr_data = data[:200]
-	imu_data = data[200+16:228]
-	terabee1_data = data[228+16:228+16+12]
-	terabee2_data = data[256+16:]
+	ldr_data = data[0:200]
+	imu_data = data[216:216+6]
+	terabee1_data = data[238:238+16]
+	terabee2_data = data[270:270+16]
+	terabee3_data = data[286+16:]
 	
 	terabee_array_append(terabee1_data, terabee_array_1)
 	terabee_array_append(terabee2_data, terabee_array_2)
+	terabee_array_append(terabee3_data, terabee_array_3)
 	lidar_tuple_array_append(ldr_data, ldr_array)
-	imu_array_append(imu_data[:6], imu_gyro_array)
-	imu_array_append(imu_data[6:], imu_accel_array)
+	imu_array_append(imu_data, imu_array)
 	
-	print("TERABEE 1:")
-	for item in terabee_array_1:
-		print(item)
-	
-	print("TERABEE 2:")
-	for item in terabee_array_2:
-		print(item)
-		
-	print("LIDAR:")
-	print(ldr_array)
-	
-	print("IMU GYRO: ")
-	print(imu_gyro_array)
-	
-	print("IMU ACCEL: ")
-	print(imu_accel_array)
 	
 def decode_from_imu(data):
 	"""
@@ -220,38 +197,18 @@ def decode_from_imu(data):
 	Returns: Nothing
 	"""
 	
-	imu_data = data[:12]
-	terabee1_data = data[12+16:28+16]
-	terabee2_data = data[44+16:60+16]
-	ldr_data = data[76+16:]
-	
-	# ~ print(terabee1_data)
-	# ~ print(terabee2_data)
-	# ~ print(ldr_data)
+	imu_data = data[0:6]
+	terabee1_data = data[22:22+16]
+	terabee2_data = data[54:54+16]
+	terabee3_data = data[86:86+16]
+	ldr_data = data[102+16:]
 	
 	terabee_array_append(terabee1_data, terabee_array_1)
 	terabee_array_append(terabee2_data, terabee_array_2)
+	terabee_array_append(terabee3_data, terabee_array_3)
 	lidar_tuple_array_append(ldr_data, ldr_array)
-	imu_array_append(imu_data[:6], imu_gyro_array)
-	imu_array_append(imu_data[6:], imu_accel_array)
-	
-	print("TERABEE 1:")
-	for item in terabee_array_1:
-		print(item)
-	
-	print("TERABEE 2:")
-	for item in terabee_array_2:
-		print(item)
-		
-	print("LIDAR:")
-	print(ldr_array)
-	
-	print("IMU GYRO: ")
-	print(imu_gyro_array)
-	
-	print("IMU ACCEL: ")
-	print(imu_accel_array)
-	
+	imu_array_append(imu_data, imu_array)
+
 def imu_array_append(data, target_array):
 	"""
 	Description: Copies the data received into the corresponding IMU array.
@@ -300,8 +257,12 @@ if __name__ == '__main__':
 		while True:
 			decode_arrays()
 			ldr = get_array('LDR')
-			for tup in ldr:
-				print(tup)
+			tb1 = get_array('TB1')
+			tb2 = get_array('TB2')
+			tb3 = get_array('TB3')
+			imu = get_array('IMU')
+			for item in imu:
+				print(item)
 
 	except KeyboardInterrupt:
 		ser.close()
