9fdfa90e67aad89c57adff98e87ce6ea82bfadc7
Stefen Pegels <sgp62@cornell.edu>
2022-05-03 20:20:15 -0400

    initial functional commit for teensy arbiter

--- arbiter_due_slave/arbiter_due_slave.ino
+++ arbiter_due_slave/arbiter_due_slave.ino
@@ -5,14 +5,20 @@
 
 #include "R2Protocol.h"
 
+#define MAX_BUFFER_SIZE 2048
+
 // parameters that r2p decode funcion takes in
-uint8_t msg_buffer[24];
-uint32_t msg_buffer_len = 24;
+uint8_t packet_in_buffer[24];
+uint8_t packet_out_buffer[24];
+uint32_t packet_in_len = 24;
 uint16_t checksum;
 char type[5];
-uint8_t msg[8];
-uint32_t msg_len;
+uint8_t msg_in[8];
+uint8_t msg_out[8] = {0,1,2,3,4,5,6,7};;
+uint32_t msg_len_in = 8;
+uint32_t msg_len_out = 8;
 
+unsigned long counter = 0;
 
 void setup() {
   Serial.begin(9600); // Serial monitor
@@ -24,19 +30,24 @@ void setup() {
   }
 }
 
+void send(char type[5], const uint8_t* msg, uint32_t msg_len, uint8_t* send_buffer) {
+  uint32_t written = r2p_encode(type, msg, msg_len, send_buffer, MAX_BUFFER_SIZE);
+  Serial1.write(send_buffer, written);
+  Serial.println("NUMBER OF BYTES WRITTEN: " + String(written));
+}
+
 void loop() {
   if (Serial1.available() > 0) {
-    //Message from Teensy, from Jetson
-    Serial1.readBytes(msg_buffer, msg_buffer_len);
-    r2p_decode(msg_buffer, msg_buffer_len, &checksum, type, msg, &msg_len);
+    //Message from arbiter
+    Serial1.readBytes(packet_in_buffer, packet_in_len);
+    r2p_decode(packet_in_buffer, packet_in_len, &checksum, type, msg_in, &msg_len_in);
 
-    for(int i = 0; i < msg_len; i++) {
-      Serial.print((char) msg[i]);
-    }
-    Serial.println();
-
-    //Send Response Message Upstream
-    
+//    for(int i = 0; i < msg_len; i++) {
+//      Serial.print((char) msg[i]);
+//    }
+//    Serial.println();
   }
-  
+  if((++counter & 2047) == 2047)
+    send("PRMR", msg_out, 8, packet_out_buffer);
+    
 }
--- arbiter_teensy_test/arbiter_teensy_test.ino
+++ arbiter_teensy_test/arbiter_teensy_test.ino
@@ -10,7 +10,7 @@ uint8_t msg_buffer[19];
 uint32_t msg_buffer_len = 19;
 uint16_t checksum;
 char type[5];
-uint8_t msg[3];
+uint8_t msg[2048];
 uint32_t msg_len;
 uint8_t recv_buf[2048];
 uint8_t iter_counter = 0;
@@ -27,12 +27,12 @@ uint8_t recv_buf2[2048], recv_buf3[2048], recv_buf4[2048], recv_buf5[2048], recv
 
 //Need to declare these for each connected device
 uint16_t msg_len3 = 24; //8 data bytes
-char type3[] = "PRM";
-char type3_t[] = "PRMT";
+char type3_d[] = "LOC"; //Downstream type
+char type3_u[] = "LOCR"; //Upstream type
 
 uint16_t msg_len4 = 24; //8 data bytes
-char type4[] = "LOC";
-char type4_t[] = "LOCT";
+char type4_d[] = "PRM"; //Downstream type
+char type4_u[] = "PRMR"; //Upstream type
   
 void setup() {
   Serial.begin(9600); // Serial monitor
@@ -40,16 +40,13 @@ void setup() {
 
   //These need to be declared in order to read and send messages to the respective devices
   Serial3.begin(115200); //DUE 1
-
   Serial4.begin(115200); //DUE 2
-
 
   while (Serial1.available() > 0) {
     Serial1.read();
     delay(100);
   }
 }
-
 
 
 inline uint8_t end_cmp(uint8_t * end_arr[3]){
@@ -64,6 +61,12 @@ void loop() {
 //  prev2 = 0xd2;
 //  delay(1000);
 //  Serial.println("Compared: " + String(end_cmp(end_arr)));
+  if(Serial3.available() > msg_len3){ //Continuously update Buffers for connected devices
+    Serial3.readBytes(recv_buf3, msg_len3);
+  }
+  if(Serial4.available() > msg_len4){ //Continuously update buffers for connected devices
+    Serial4.readBytes(recv_buf4, msg_len4);
+  }
 
   if (Serial1.available() > 17) { //At least one message has arrived, smallest message is 17 bytes (with new R2P)
     //Serial.println(Serial1.available());
@@ -76,31 +79,38 @@ void loop() {
       buf_idx++;
     }while(Serial1.available() && !end_cmp(end_arr)); //While there are more bytes and we haven't reached the stop sequence
 
+    
+    
     if(end_cmp(end_arr)) {
       msg_buffer_len = buf_idx;
       msg_len = msg_buffer_len-16;//17 with new r2p
       //Serial.println(msg_len);
       buf_idx = 0;
+
+//      for(int i = 0; i < msg_buffer_len; i++) {
+//       Serial.print(recv_buf[i]);
+//      }
+//      Serial.println();
       
       //Serial1.readBytes(recv_buf, msg_buffer_len);
       r2p_decode(recv_buf, msg_buffer_len, &checksum, type, msg, &msg_len);
       //Route ENCODED data to correct host, or print out type if not resolved
       
-      if(!strcmp(type,type3)){ //Downstream Serial3
+      if(!strcmp(type,type3_d)){ //Downstream Serial3
         Serial3.write(recv_buf, msg_buffer_len);
         Serial.println("Wrote "+String(type)+" message");
       }
-      else if(!strcmp(type,type3_t)){ //Upstream Serial3
-        Serial3.readBytes(recv_buf3, msg_len3);
-        Serial.println("Received "+String(type)+" data");
+      else if(!strcmp(type,type3_u)){ //Upstream Serial3
+        Serial1.write(recv_buf3, msg_len3);
+        Serial.println("Wrote "+String(type)+" data upstream");
       }
-      else if(!strcmp(type,type4)){ //Downstream Serial4
+      else if(!strcmp(type,type4_d)){ //Downstream Serial4
         Serial4.write(recv_buf, msg_buffer_len);
         Serial.println("Wrote "+String(type)+" message");
       }
-      else if(!strcmp(type,type4_t)){ //Upstream Serial4
-        Serial4.readBytes(recv_buf4, msg_len4);
-        Serial.println("Received "+String(type)+" data");
+      else if(!strcmp(type,type4_u)){ //Upstream Serial4
+        Serial1.write(recv_buf4, msg_len4);
+        Serial.println("Wrote "+String(type)+" data upstream");
       }
       else{
         Serial.println("Unrecognized Type: "+String(type));
