63c4c9763ea33481b2d6a176b964ccab406b34fb
c1c0-main <jwt95@cornell.edu>
2022-05-05 20:05:26 -0400

    testing with arbiter

--- arbiter_test.py
+++ arbiter_test.py
@@ -7,35 +7,43 @@
 import serial
 import time
 import sys
+import TEST_API
 
 sys.path.append('/home/c1c0-main/c1c0-movement/c1c0-movement/Locomotion')
 #Resolve 'cornellcup' to be the username of this Jetson, make sure c1c0-movement is cloned
 import R2Protocol2 as r2p
 
-ser = serial.Serial(
-    port = '/dev/ttyTHS1', #Jetson hardware serial port (pins 8/10 on header)
-    baudrate = 115200, #Bits/s data rate
-)
+# ~ ser = serial.Serial(
+    # ~ port = '/dev/ttyTHS1', #Jetson hardware serial port (pins 8/10 on header)
+    # ~ baudrate = 115200, #Bits/s data rate
+# ~ )
 
-data1 = "123"
+ser = TEST_API.init_serial('/dev/ttyTHS1', 115200)
+
+data1 = [0,5,10,15,20,25]
 data2 = "1234567"
 data3 = "12345"
+
+ser.reset_input_buffer()
 
 def send_token(msg_type, token):
     req = r2p.encode(msg_type, token.to_bytes(1, 'big'))
     ser.write(req)
+    
+def convert_8_to_16(msg, length):
+    data = []
+    for i in range(0,length,2):
+        data.append((msg[i] << 8) | msg[i+1]) 
+    return data
 
 while(True):
     
-    msg = r2p.encode(b"PRM", bytearray(data1, "utf-8"))
-
+    msg = r2p.encode(b"PRM", bytes(data1))
     ser.write(msg) 
     print("Wrote PRM Data Downstream")
-    
     msg = r2p.encode(b"sprt", bytearray(data2, "utf-8"))
-
     ser.write(msg)
-    print("Wrote sprt Data Upstream")
+    print("Wrote sprt Data Downstream")
     
     
     send_token(b"LOCR", 1)
@@ -46,16 +54,23 @@ while(True):
     print(ser_msg)
     
     msg = r2p.encode(b"LOC", bytearray(data3, "utf-8")) 
-
     ser.write(msg) 
     print("Wrote LOC Data Downstream")
     
     
     send_token(b"PRMR", 1)
     print("Requested PRM Data Downstream")
-    ser_msg = ser.read(24)
+    ser_msg = ser.read(28)
     mtype, data, status = r2p.decode(ser_msg)
     print("Received PRM Data Upstream")
-    print(ser_msg)
+    print(convert_8_to_16(data,12))
+    
+    
+    TEST_API.sensor_token(b"SNSR",1)
+    print("Requested SNS Data Downstream")
+    TEST_API.decode_arrays()
+    imu = get_array('IMU')
+    print(imu)
+    print("Received SNS Data Upstream")
     
     time.sleep(1)
