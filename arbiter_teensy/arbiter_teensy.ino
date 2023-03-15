0c0c8885359783c6ae468fff6678322e34b258fb
Rachel Yan <sy625@cornell.edu>
2023-03-15 18:24:05 -0400

    added IMU code to arbiter_teensy.ino

--- arbiter_teensy/arbiter_teensy.ino
+++ arbiter_teensy/arbiter_teensy.ino
@@ -4,7 +4,17 @@
 // https://docs.google.com/document/d/1cKZGTqvyOFYL5ugZGULJ74xGuEwgBEWSXJAztM2qnaE/edit
 // Author: Stefen Pegels, email sgp62@cornell.edu for contact
 
+/*  I2C Info
+ *  IMU Device: Adafruit BNO055
+ *  Device address ->
+ */
+
 #include "R2Protocol.h"
+/* ADDED IMU SETTING*/
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <EEPROM.h>
+
 //Uncomment which serial ports are in use, make sure to define their parameters below
 //#define SER2
 #define SER3
@@ -12,6 +22,19 @@
 //#define SER5
 //#define SER6
 //#define SER7
+
+//imu define
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+//TEMPORARILY HERE, TRYING TO CLEAN UP SO NO OTHER DATA INVOLVED
+#define MAX_BUFFER_SIZE 2048
+
+//THIS IS IMPORTANT
+#define IMU_DATA_LEN 6
+
+//ORIGINALLY FOR SENSOR DATA BUFFER, DON'T THINK WE SHOULD NEED IT.
+#define IMU_START TERABEE_DATA_LEN * 3 + LIDAR_DATA_LEN
+
 
 //Need to declare these for each connected device
 
@@ -70,6 +93,7 @@ uint16_t ser7_count = 0;
 #endif
 
 
+
 // parameters for r2p decode from jetson downstream
 uint32_t msg_buffer_len = 0;
 uint16_t checksum;
@@ -86,8 +110,90 @@ uint8_t *p_prev2=&prev2, *p_prev1=&prev1, *p_prev0=&prev0;
 uint8_t end_seq[3] = {0xd2,0xe2,0xf2};
 uint8_t * end_arr[3] = {p_prev2, p_prev1, p_prev0};
 
+// IMU variables
+/*This IS IMPORTANT, added IMU code*/
+sensors_event_t event;
+uint8_t imu_databuffer[6];
+uint16_t imu_data[3];
+bool foundCalib;
 
-  
+Adafruit_BNO055 bno = Adafruit_BNO055(55); // Instantiate IMU
+
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+}
+
+/**************************************************************************/
+/*
+    Display some basic info about the sensor status
+    */
+/**************************************************************************/
+void displaySensorStatus(void)
+{
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
+}
+
+/**************************************************************************/
+/*
+    Display sensor calibration status
+    */
+/**************************************************************************/
+void displayCalStatus(void)
+{
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  /* The data should be ignored until the system calibration is > 0 */
+  Serial.print("\t");
+  if (!system)
+  {
+    Serial.print("! ");
+  }
+
+    /* Display the individual values */
+  Serial.print("Sys:");
+  Serial.print(system, DEC);
+  Serial.print(" G:");
+  Serial.print(gyro, DEC);
+  Serial.print(" A:");
+  Serial.print(accel, DEC);
+  Serial.print(" M:");
+  Serial.print(mag, DEC);
+}
+
+/**************************************************************************/
+/*
+    Display the raw calibration offset and radius data
+    */
+/**************************************************************************/
+void displaySensorOffsets(const adafruit_bno055_offsets_t &calibData)
+{
+}
+
+void convert_b16_to_b8(uint16_t *databuffer, uint8_t *data, int len)
+{
+  //  int data_idx1;
+  //  int data_idx2;
+  for (int i = 0; i < 2 * len; i += 2)
+  {
+    data[i] = (databuffer[i / 2] >> 8) & 255;
+    data[i + 1] = (databuffer[i / 2]) & 255;
+  }
+}
+/*
+end of added IMU code uphere
+*/
+
 void setup() {
   
   Serial.begin(115200); // Serial monitor
@@ -113,10 +219,133 @@ void setup() {
   Serial7.begin(115200);
   #endif
 
+  bno.begin(); // IMU Initialization
+  bno.enterNormalMode();
+
   while (Serial1.available() > 0) {
     Serial1.read();
     delay(100);
   }
+
+  /*
+  Added IMU code
+  */
+  Serial.println("Orientation Sensor Test");
+  Serial.println("");
+
+  /* Initialise the sensor */
+  //  if (!bno.begin())
+  //  {
+  //    /* There was a problem detecting the BNO055 ... check your connections */
+  //    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+  //    while (1)
+  //      ;
+  //  }
+
+  int eeAddress = 0;
+  long bnoID;
+  bool foundCalib = false;
+
+  EEPROM.get(eeAddress, bnoID);
+
+  adafruit_bno055_offsets_t calibrationData;
+  sensor_t sensor;
+
+  /*
+   *  Look for the sensor's unique ID at the beginning oF EEPROM.
+   *  This isn't foolproof, but it's better than nothing.
+   */
+  bno.getSensor(&sensor);
+  if (bnoID != sensor.sensor_id)
+  {
+    Serial.println("\nNo Calibration Data for this sensor exists in EEPROM");
+    delay(500);
+  }
+  else
+  {
+    Serial.println("\nFound Calibration for this sensor in EEPROM.");
+    eeAddress += sizeof(long);
+    EEPROM.get(eeAddress, calibrationData);
+
+    displaySensorOffsets(calibrationData);
+
+    Serial.println("\n\nRestoring Calibration data to the BNO055...");
+    bno.setSensorOffsets(calibrationData);
+
+    Serial.println("\n\nCalibration data loaded into BNO055");
+    foundCalib = true;
+  }
+
+  delay(1000);
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+
+  /* Optional: Display current status */
+  displaySensorStatus();
+
+  /* Crystal must be configured AFTER loading calibration data into BNO055. */
+  bno.setExtCrystalUse(true);
+
+  /* always recal the mag as It goes out of calibration very often */
+  //  if (foundCalib)
+  //  {
+  //    Serial.println("Move sensor slightly to calibrate magnetometers");
+  //    while (!bno.isFullyCalibrated())
+  //    {
+  //      bno.getEvent(&event);
+  //      delay(BNO055_SAMPLERATE_DELAY_MS);
+  //    }
+  //  }
+  //  else
+  //  {
+  //    Serial.println("Please Calibrate Sensor: ");
+  //    while (!bno.isFullyCalibrated())
+  //    {
+  //      bno.getEvent(&event);
+  //
+  //      Serial.print("X: ");
+  //      Serial.print(event.orientation.x, 4);
+  //      Serial.print("\tY: ");
+  //      Serial.print(event.orientation.y, 4);
+  //      Serial.print("\tZ: ");
+  //      Serial.print(event.orientation.z, 4);
+  //
+  //      /* Optional: Display calibration status */
+  //      displayCalStatus();
+  //
+  //      /* New line for the next sample */
+  //      Serial.println("");
+  //
+  //      /* Wait the specified delay before requesting new data */
+  //      delay(BNO055_SAMPLERATE_DELAY_MS);
+  //    }
+  //  }
+  //
+  //  Serial.println("\nFully calibrated!");
+  //  Serial.println("--------------------------------");
+  //  Serial.println("Calibration Results: ");
+  //  adafruit_bno055_offsets_t newCalib;
+  //  bno.getSensorOffsets(newCalib);
+  //  displaySensorOffsets(newCalib);
+  //
+  //  Serial.println("\n\nStoring calibration data to EEPROM...");
+  //
+  //  eeAddress = 0;
+  //  bno.getSensor(&sensor);
+  //  bnoID = sensor.sensor_id;
+  //
+  //  EEPROM.put(eeAddress, bnoID);
+  //
+  //  eeAddress += sizeof(long);
+  //  EEPROM.put(eeAddress, newCalib);
+  //  Serial.println("Data stored to EEPROM.");
+  //
+  //  Serial.println("\n--------------------------------\n");
+  //  delay(500);
+  /*
+  End of added IMU code in setup
+  */
 }
 
 
@@ -165,8 +394,10 @@ uint32_t msg_buffer_len3 = 0;
 uint16_t checksum3;
 char type3[5];
 uint8_t msg3[2048];
-uint32_t msg_len3;
-uint8_t recv_buf3[2048];
+//CONFLICT DEFINITION WITH uint16_t msg_len3 = 1574; //upstream packet length (data len) + 16, COMMENT OUT RIGHT NOW
+//uint32_t msg_len3;
+//CONFLICT DEFINITION WITH THE SAME THING ABOVE, COMMENT OUT FOR NOW
+//uint8_t recv_buf3[2048];
 //Sensor serial port
 #ifdef SER3
 void serialEvent3(){
@@ -201,7 +432,7 @@ void serialEvent3(){
   if(ser3_count == msg_len3) {
 //    Serial.println(ser3_count);
 //    Serial.println(recv_buf3[1573]);
-    r2p_decode(recv_buf3, msg_len3, &checksum3, type3, msg3, &msg_len3);
+    //r2p_decode(recv_buf3, msg_len3, &checksum3, type3, msg3, &msg_len3);
     ser3_count = 0;
 
     STATE3 = 0;
@@ -364,6 +595,20 @@ void serialEvent1(){
 
 }
 
+/*
+Added IMU_send buffer
+*/
+uint8_t imu_send_buffer[MAX_BUFFER_SIZE];
+
+/*
+Add send function, need it for sending IMU data
+*/
+void send(char type[5], const uint8_t *data, uint32_t data_len, uint8_t *send_buffer)
+{
+  uint32_t written = r2p_encode(type, data, data_len, send_buffer, MAX_BUFFER_SIZE);
+  Serial4.write(send_buffer, written);
+  //Serial.println("NUMBER OF BYTES WRITTEN! READ ME " + String(written));
+}
 
 void loop() {
   //Successfully found a packet from the jetson
@@ -450,6 +695,20 @@ void loop() {
       Serial.println("Wrote "+String(type)+" data upstream");
     }
     #endif
+    /*
+    Below all added code for IMU
+    */
+    Serial.println("Reading imu");
+    bno.getEvent(&event);
+    imu_data[0] = (int)event.orientation.x;
+    imu_data[1] = (int)event.orientation.y;
+    imu_data[2] = (int)event.orientation.z;
+
+    //convert_b16_to_b8(imu_data, imu_databuffer+IMU_START, 3);
+    convert_b16_to_b8(imu_data, imu_databuffer, 3);
+    if (1){
+      send("IMU", imu_databuffer, 6, imu_send_buffer);
+    }
 //      else{
 //        Serial.println("Unrecognized Type: "+String(type));
 //      }
